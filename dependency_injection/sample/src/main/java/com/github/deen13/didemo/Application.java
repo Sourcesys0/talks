f94ac136519be9e40904f1443fea11d7b88a4874
i15846 <dennis.dierkes@itelligence.de>
2017-10-06 11:22:54 +0200

    base sample project

---
+++ dependency_injection/sample/src/main/java/com/github/deen13/didemo/Application.java
@@ -0,0 +1,13 @@
+package com.github.deen13.didemo;
+
+import static org.springframework.boot.SpringApplication.run;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		run(Application.class, args);
+	}
+	
+}
