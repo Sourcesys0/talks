a3612d5c0367f2fe7a6ffdb1560b7ba10a67c725
i15846 <dennis.dierkes@itelligence.de>
2017-11-16 10:15:34 +0100

    di sample moved to https://github.com/deen13/javafx-spring-testing

--- dependency_injection/sample/src/main/java/com/github/deen13/ui/NewsView.java
+++
@@ -1,9 +0,0 @@
-package com.github.deen13.ui;
-
-import de.felixroske.jfxsupport.AbstractFxmlView;
-import de.felixroske.jfxsupport.FXMLView;
-
-@FXMLView(title = "News")
-public class NewsView extends AbstractFxmlView {
-
-}
